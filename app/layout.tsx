ccb3599fb8a887bc41567dd3eda26cee67ce2066
rasel3980 <rkrasel3980@gmail.com>
2026-03-15 18:36:27 +0600

    added redux recipe slice

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/shared/Navbar";
 import Footer from "./components/shared/Footer";
-import ReduxProvider from "./store/Provider";
+import ReduxProvider from "./redux/Provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
