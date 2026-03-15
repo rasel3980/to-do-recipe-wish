86522955ab76ec5c91c64905fc32a873ff6821c8
rasel3980 <rkrasel3980@gmail.com>
2026-03-15 22:32:01 +0600

    added Meal API types

---
+++ app/types/recipe.ts
@@ -0,0 +1,10 @@
+export interface MealAPI {
+    idMeal: string,
+    strMeal: string,
+    strCategory: string,
+    strArea: string,
+    strInstructions:string,
+    strMealThumb:string,
+    strYoutube:string,
+    
+}
