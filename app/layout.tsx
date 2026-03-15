6be69a588c20c58a5230a813f92c847b32a554ec
rasel3980 <rkrasel3980@gmail.com>
2026-03-15 18:02:55 +0600

    Redux provider wrap in main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/shared/Navbar";
 import Footer from "./components/shared/Footer";
+import ReduxProvider from "./store/Provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar></Navbar>
-        {children}
-        <Footer></Footer>
+        <ReduxProvider>
+          <Navbar></Navbar>
+          {children}
+          <Footer></Footer>
+        </ReduxProvider>
       </body>
     </html>
   );
