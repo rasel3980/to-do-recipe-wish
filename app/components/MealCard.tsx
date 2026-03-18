3d932facf6d933d8f018d26ebc6e3c45e436b258
rasel3980 <rkrasel3980@gmail.com>
2026-03-18 23:19:55 +0600

    added wishList page

--- app/components/MealCard.tsx
+++ app/components/MealCard.tsx
@@ -1,12 +1,29 @@
 "use client";
 import { MealAPI } from "@/app/types/recipe";
 import Image from "next/image";
+import { useAppDispatch } from "../redux/store/hooks";
+import { addToWishlist } from "../redux/features/recipes/recipeSlice";
 
 interface MealCardProps {
   meal: MealAPI;
 }
 
 const MealCard = ({ meal }: MealCardProps) => {
+  const dispatch = useAppDispatch();
+
+  const handleAddToWishlist = () => {
+    dispatch(
+      addToWishlist({
+        id: meal.idMeal,
+        name: meal.strMeal,
+        category: meal.strCategory,
+        thumbnail: meal.strMealThumb,
+        cooked: false,
+        addedAt: new Date().toISOString(),
+      })
+    );
+  };
+
   return (
     <div className="border rounded-lg overflow-hidden shadow-md">
       <Image
@@ -20,8 +37,11 @@ const MealCard = ({ meal }: MealCardProps) => {
         <h2 className="text-lg font-bold">{meal.strMeal}</h2>
         <p className="text-sm text-gray-500">{meal.strCategory}</p>
         <p className="text-sm text-gray-500">{meal.strArea}</p>
-        <button className="mt-3 w-full bg-orange-500 text-white py-2 rounded-lg">
-          Add to Wishlist
+        <button
+          onClick={handleAddToWishlist}
+          className="mt-3 w-full bg-orange-500 text-white py-2 rounded-lg hover:bg-orange-600"
+        >
+          ❤️ Add to Wishlist
         </button>
       </div>
     </div>
---
+++ app/wishList/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+import WishListCard from "../components/WishListCard";
+import { useAppSelector } from "../redux/store/hooks";
+
+export default function WishlistPage() {
+  const wishlist = useAppSelector((state) => state.recipes.wishlist);
+
+  if (wishlist.length === 0) {
+    return (
+      <div className="text-center py-20">
+        <p className="text-2xl">😢 Wishlist Empty</p>
+        <p className="text-gray-500 mt-2">
+         Recipe add from Homne
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <main>
+      <h1 className="text-3xl font-bold text-center py-6">
+        ❤️ My Wishlist ({wishlist.length})
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
+        {wishlist.map((recipe) => (
+          <WishListCard key={recipe.id} recipe={recipe} />
+        ))}
+      </div>
+    </main>
+  );
+}
