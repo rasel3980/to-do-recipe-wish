d01e7d79f0da1ee42c12d1449cc03d773c8cf475
rasel3980 <rkrasel3980@gmail.com>
2026-03-21 16:08:04 +0600

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import MealList from "./components/MealList";
 export default function Home() {
   return (
     <>
+    <h1 className="text-3xl font-bold flex justify-center items-center my-5">Choose for your wishlist</h1>
       <MealList></MealList>
       </>
   );
